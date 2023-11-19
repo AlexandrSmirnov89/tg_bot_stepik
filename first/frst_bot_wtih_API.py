699737cfba548f32c4aee3d051fb535a040cf35e
Alexandr Smirnov <smirnov1sasha@gmail.com>
2023-11-20 01:02:26 +0500

    создана программа использующая API, которая выводит интересную инфу о числе

--- first/frst_bot_wtih_API.py
+++ first/frst_bot_wtih_API.py
@@ -1,4 +1,28 @@
 import requests
 import time
+import token_my_bot
 
 
+TOKEN = token_my_bot.token
+URL_TG = 'https://api.telegram.org/bot'
+TEXT_MESSAGE = 'ЖОПА'
+MAX_COUNT = 100
+
+offset = -2
+counter = 0
+
+
+while counter < MAX_COUNT:
+    print(counter)
+
+    updates = requests.get(f'{URL_TG}{TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for update in updates['result']:
+            print(update)
+            offset = update['update_id']
+            chat_id = update['message']['from']['id']
+            requests.get(f'{URL_TG}{TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT_MESSAGE}')
+
+    time.sleep(1)
+    counter += 1
